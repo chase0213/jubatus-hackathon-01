cf5cf57e91d9ccc0d78b3ae40dc8daff6cc12a5b
chase0213 <chase0213@gmail.com>
2014-10-04 01:15:49 +0900

    setting up jubatus

---
+++ jubatus/update.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+host = '127.0.0.1'
+port = 9199
+name = 'dmp'
+
+import jubatus
+from jubatus.common import Datum
+
+import json
+
+LEARNING_JSON_PATH = './ads.json'
+
+
+def connect_jubatus():
+    client = jubatus.Recommender(host, port, name)
+    return client
+
+
+def update(learning_json_path):
+    client = connect_jubatus()
+    with open(learning_json_path) as f:
+        learning_data = json.load(f)
+    for key, value in learning_data.items():
+        d = Datum(value)
+        client.update_row(key,d)
+
+def main():
+    update(LEARNING_JSON_PATH)
+
+if __name__ == '__main__':
+    main()
